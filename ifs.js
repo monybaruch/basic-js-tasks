6a336cd89ac1d4cbaf096105da060a236d1528f2
מוני ברוך <monybaruch@gmail.com>
2023-03-03 17:17:27 +0200

    adding a leap year challenge

--- ifs.js
+++ ifs.js
@@ -45,22 +45,60 @@
 // BMI 18.5-24.9, the output should be "Your BMI is <bmi>, so you have a normal weight."
 
 // BMI >24.9, the output should be "Your BMI is <bmi>, so you are overweight."
-var yourWeight = prompt("enter your weight !");
-var yourHeight = prompt("enter your height !");
-var yourBmi = "";
+// var yourWeight = prompt("enter your weight !");
+// var yourHeight = prompt("enter your height !");
+// var yourBmi = "";
 
-function bmiCalculator(myWeight, myHeight) {
-  let bmi = myWeight / (myHeight * myHeight);
-  bmi = Math.round(bmi);
-  if (bmi > 30) {
-    yourBmi = "Your BMI is " + bmi + " you are really fat go for a run";
-  } else if (bmi >= 25 && 29.9 <= bmi) {
-    yourBmi = "Your BMI is " + bmi + " you are still fat go for a walk";
-  } else if (bmi >= 18.5 && 24.9 <= bmi) {
-    yourBmi = "Your BMI is " + bmi + " you are not fat so good for you";
+// function bmiCalculator(myWeight, myHeight) {
+//   let bmi = myWeight / (myHeight * myHeight);
+//   bmi = Math.round(bmi);
+//   if (bmi > 30) {
+//     yourBmi = "Your BMI is " + bmi + " you are really fat go for a run";
+//   } else if (bmi >= 25 && 29.9 <= bmi) {
+//     yourBmi = "Your BMI is " + bmi + " you are still fat go for a walk";
+//   } else if (bmi >= 18.5 && 24.9 <= bmi) {
+//     yourBmi = "Your BMI is " + bmi + " you are not fat so good for you";
+//   } else {
+//     yourBmi = "Your BMI is " + bmi + " you are to skinny go eat some KFC";
+//   }
+//   return alert(yourBmi);
+// }
+// bmiCalculator(yourWeight, yourHeight);
+
+// leap year challenge :
+// Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February. The reason why we have leap years is really fascinating, this video goes into more detail.
+
+// This is how to work out whether if a particular year is a leap year:
+
+// A year is a leap year if it is evenly divisible by 4 ;
+
+// except if that year is also evenly divisible by 100;
+
+// unless that year is also evenly divisible by 400.
+
+// e.g. Is the year 2000 a leap year?:
+
+// 2000 ÷ 4 = 500 (Leap)
+
+// 2000 ÷ 100 = 20 (Not Leap)
+
+// 2000 ÷ 400 = 5 (Leap!)
+
+var leapYear = prompt("enter the year you want to check if it's a leap year! ");
+
+function YearLeapCalculator(myYear) {
+  if (myYear % 4 === 0) {
+    if (myYear % 100 === 0) {
+      if (myYear % 400 === 0) {
+        alert("The year you chose is a leap year");
+      } else {
+        alert("The year you chose is not a leap year");
+      }
+    } else {
+      alert("The year you chose is a leap year");
+    }
   } else {
-    yourBmi = "Your BMI is " + bmi + " you are to skinny go eat some KFC";
+    alert("The year you chose is not a leap year");
   }
-  return alert(yourBmi);
 }
-bmiCalculator(yourWeight, yourHeight);
+YearLeapCalculator(leapYear);
